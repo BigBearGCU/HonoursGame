6ab621bfe015d8eff0e977dcae3e256f1e7c665f
Michele Tammaro <mtamma10@caledonian.ac.uk>
2014-02-26 23:03:21 +0000

    First Commit 26/02
    First Commit

---
+++ New Unity Project 1/Assets/GUI/GUI_Button_Elephant.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUI_Button_Elephant : MonoBehaviour {
+
+	// Use this for initialization
+	public Texture2D butImg = null;
+	GameObject ele;
+
+	void Start () {
+		ele = GameObject.Find ("Elephant_Mother");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void OnGUI()
+	{
+		if (GUI.Button (new Rect (145, Screen.height - 75, butImg.width, butImg.height), butImg)) 
+		{
+			ele.transform.position += new Vector3(-0.5f, 0.0f, -1.5f);
+			
+		}
+	}
+}
